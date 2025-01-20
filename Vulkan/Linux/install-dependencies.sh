aab9c5b731097a7e00f5c5b979530d2d482f977c
Mattia Massarenti <mattia.mas2003@gmail.com>
2025-01-20 22:56:22 +0100

    update

--- Vulkan/Linux/install-dependencies.sh
+++ Vulkan/Linux/install-dependencies.sh
@@ -1,10 +1,9 @@
 #!/bin/bash
 
-echo "Installing dependencies...\n\n"
+echo -e "\033[1mInstalling dependencies...\n\n\033[0m"
 
 sudo apt install vulkan-tools libvulkan-dev vulkan-validationlayers-dev spirv-tools libglfw3-dev libglm-dev
 
 vulkaninfo
+echo -e "\033[1m** If you don't see Vulkan information or the cube, your system doesn't support Vulkan **\n\033[0m"
 vkcube
-
-echo "If you don't see Vulkan information or the cube, your system doesn't support Vulkan.\n"
